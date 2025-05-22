6e6006caccef9682a561c93e95b9ccbe1bdc4e61
Augusto Guilherme Pasqualotto <guto2408@gmail.com>
2025-05-22 16:57:21 -0300

    adicionando .gitignore e funções do js

--- estudo.js
+++ estudo.js
@@ -39,7 +39,7 @@ console.log(carro2.marca);
 //serve também com arrays
 
 nomes = ["augusto", "joao", "paulo"]
-nomes2 = [...nomes, "pedro"]
+nomes2 = [...nomes, "pedro", "claudio"]
 
 console.log(nomes2);
 
@@ -69,7 +69,7 @@ console.log(nomesMap); //retorna um array com os nomes seguidos de "1"
 
 //filter cria um novo array com os valores que passaram por uma condição específica
 
-numeros = [1,2,3,4,5,6]
+const numeros = [1,2,3,4,5,6]
 
 const pares = numeros.filter((num) => {
     return num % 2 == 0;
@@ -77,3 +77,41 @@ const pares = numeros.filter((num) => {
 
 console.log(pares); //retorna um array apenas de números pares
 
+//forEach executa uma função para cada item do array, sem criar um array novo
+
+numeros.forEach(num => {
+    console.log(num / 2)
+});
+
+//find retorna o primeiro item que satisfaz uma condição(ou undefined)
+
+const find = nomes.find((n) => n.startsWith("a"));
+console.log(find);
+
+//sort Ordena os itens modificando o array original
+
+nomes2.sort().reverse();
+console.log(nomes2);
+
+//some(algum item satisfaz a condição) / every(todos os itens satisfazem a condição)
+
+const some = numeros.some((n) => n % 2 == 0);
+console.log(some); 
+
+const every = numeros.every((n) => n > 0);
+console.log(every);
+
+//reduce serve para reduzir o array e um único valor, usando um acumulador
+
+const rockets = [
+    {country: "Russia", launches: 32},
+    {country: "US", launches: 23},
+    {country: "China", launches: 16},
+    {country: "Europe", launches: 7},
+    {country: "India", launches: 4},
+    {country: "Japan", launches: 3},
+]
+
+const total = rockets.reduce((prevVal, elem) => prevVal + elem.launches, 0)
+
+console.log(total);
